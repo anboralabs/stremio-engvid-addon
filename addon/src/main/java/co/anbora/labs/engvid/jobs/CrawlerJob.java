e72f79f642c2721c298ba0dba8138f9750786282
Darwin Algarin <dalgarins@live.com>
2020-02-18 22:54:26 -0500

    finish Crawler job to save remote videos in local db

--- addon/src/main/java/co/anbora/labs/engvid/jobs/CrawlerJob.java
+++ addon/src/main/java/co/anbora/labs/engvid/jobs/CrawlerJob.java
@@ -1,11 +1,29 @@
 package co.anbora.labs.engvid.jobs;
 
+import javax.inject.Inject;
 import javax.inject.Singleton;
+
+import co.anbora.labs.engvid.domain.usecase.UseCaseExecutor;
+import co.anbora.labs.engvid.domain.usecase.lesson.SyncRemoteLessonsUseCase;
 import io.micronaut.scheduling.annotation.Scheduled;
+
+import java.util.function.Function;
 
 @Singleton
 public class CrawlerJob {
 
+    @Inject
+    UseCaseExecutor useCaseExecutor;
+
+    @Inject
+    SyncRemoteLessonsUseCase syncRemoteLessonsUseCase;
+
     @Scheduled(fixedRate = "5m")
-    public void process() {}
+    public void process() {
+        useCaseExecutor.execute(
+                syncRemoteLessonsUseCase,
+                new SyncRemoteLessonsUseCase.Request(),
+                Function.identity()
+        );
+    }
 }
