4b59256ee8b9a3ca496fd27b5788334b7aba8338
Darwin Algarin <dalgarins@live.com>
2020-02-20 16:51:35 -0500

    Changed way to get Preview Image

--- data/src/main/java/co/anbora/labs/engvid/data/remote/mapper/HtmlToLessonMediaMapper.java
+++ data/src/main/java/co/anbora/labs/engvid/data/remote/mapper/HtmlToLessonMediaMapper.java
@@ -1,5 +1,6 @@
 package co.anbora.labs.engvid.data.remote.mapper;
 
+import co.anbora.labs.engvid.data.remote.builders.MediaBuilder;
 import co.anbora.labs.engvid.domain.constants.HtmlConstants;
 import co.anbora.labs.engvid.domain.model.lesson.LessonMedia;
 import org.jsoup.Jsoup;
@@ -9,21 +10,17 @@ import org.jsoup.nodes.Element;
 import java.util.Objects;
 import java.util.function.BiFunction;
 
-import static co.anbora.labs.engvid.domain.constants.Constants.EMPTY_VALUE;
-import static co.anbora.labs.engvid.domain.constants.EnglishVideoConstants.YOUTUBE_EMBED_URL;
-
 public class HtmlToLessonMediaMapper implements BiFunction<String, Long, LessonMedia> {
     @Override
     public LessonMedia apply(String html, Long lessonId) {
         if (Objects.nonNull(html)) {
             Document mediaHtml = Jsoup.parse(html);
             Element video = getElement(mediaHtml, HtmlConstants.TWITTER_ATTR_PLAYER);
-            Element image = getElement(mediaHtml, HtmlConstants.TWITTER_ATTR_IMAGE);
 
             return LessonMedia.builder()
                     .id(lessonId)
-                    .imageUrl(image.attr(HtmlConstants.CONTENT))
-                    .youtubeId(video.attr(HtmlConstants.CONTENT).replace(YOUTUBE_EMBED_URL, EMPTY_VALUE))
+                    .imageUrl(MediaBuilder.imageUrl(video))
+                    .youtubeId(MediaBuilder.videoId(video))
                     .sync(true)
                     .build();
         }
