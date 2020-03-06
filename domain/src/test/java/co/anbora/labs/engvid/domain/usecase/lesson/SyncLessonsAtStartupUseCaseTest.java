a78793fe55f837d6f7ab67ff29b0999a729b783b
Darwin Algarin <dalgarins@live.com>
2020-03-05 19:02:30 -0500

    added sync lesson at startup test for use case

--- domain/src/test/java/co/anbora/labs/engvid/domain/usecase/lesson/SyncLessonsAtStartupUseCaseTest.java
+++ domain/src/test/java/co/anbora/labs/engvid/domain/usecase/lesson/SyncLessonsAtStartupUseCaseTest.java
@@ -1,12 +1,31 @@
 package co.anbora.labs.engvid.domain.usecase.lesson;
 
+import co.anbora.labs.engvid.domain.CommonValuesForTests;
+import co.anbora.labs.engvid.domain.repository.IRepository;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 
-import static org.junit.Assert.*;
-
+@RunWith(MockitoJUnitRunner.class)
 public class SyncLessonsAtStartupUseCaseTest {
+
+    private IRepository repository = Mockito.mock(IRepository.class);
+    private SyncLessonsAtStartupUseCase useCase;
 
     @Before
     public void setUp() throws Exception {
+
+        this.useCase = new SyncLessonsAtStartupUseCase(repository);
+    }
+
+    @Test
+    public void returnAllLessonFromRepository() {
+        Mockito.when(repository.getLessons())
+                .thenReturn(CommonValuesForTests.advancedLessons());
+        SyncLessonsAtStartupUseCase.Response response = this.useCase.execute(new SyncLessonsAtStartupUseCase.Request());
+        Assert.assertEquals(response.getLessons(), CommonValuesForTests.advancedLessons());
     }
 }
