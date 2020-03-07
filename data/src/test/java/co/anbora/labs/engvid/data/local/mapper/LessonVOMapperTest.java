f050837da38e1505d2e6179c96f321e16f9576b7
Darwin Algarin <dalgarins@live.com>
2020-03-06 19:05:51 -0500

    added test cases for LessonVOMapper

--- data/src/test/java/co/anbora/labs/engvid/data/local/mapper/LessonVOMapperTest.java
+++ data/src/test/java/co/anbora/labs/engvid/data/local/mapper/LessonVOMapperTest.java
@@ -1,12 +1,67 @@
 package co.anbora.labs.engvid.data.local.mapper;
 
+import co.anbora.labs.engvid.data.local.model.LessonVO;
+import co.anbora.labs.engvid.domain.model.Lesson;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class LessonVOMapperTest {
 
+    private LessonVOMapper mapper;
+
     @Before
     public void setUp() throws Exception {
+
+        mapper = new LessonVOMapper();
     }
+
+    @Test
+    public void givenALessonVONullReturnNull() {
+
+        Assert.assertNull(mapper.apply(null));
+    }
+
+    @Test
+    public void givenALessonVOWithOutYoutubeIDReturnLessonNotSync() {
+
+        LessonVO lessonVO = new LessonVO(
+            1L,  "test", "test",
+                null, null, null, null,
+                null, null, null
+        );
+
+        Lesson lesson = Lesson.builder()
+                .id(1L)
+                .title("test")
+                .description("test")
+                .sync(false)
+                .build();
+
+        Assert.assertEquals(lesson, mapper.apply(lessonVO));
+    }
+
+    @Test
+    public void givenALessonVOWithYoutubeIDReturnLessonSync() {
+
+        LessonVO lessonVO = new LessonVO(
+                1L,  "test", "test",
+                null, null, null, null,
+                null, "youtubeId", null
+        );
+
+        Lesson lesson = Lesson.builder()
+                .id(1L)
+                .title("test")
+                .description("test")
+                .youtubeId("youtubeId")
+                .sync(true)
+                .build();
+
+        Assert.assertEquals(lesson, mapper.apply(lessonVO));
+    }
+
+
 }
