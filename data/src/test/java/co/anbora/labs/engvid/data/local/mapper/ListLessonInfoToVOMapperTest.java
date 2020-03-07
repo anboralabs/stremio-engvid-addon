bc01fdbe86b1d8961166bc80d1d817cad6e02972
Darwin Algarin <dalgarins@live.com>
2020-03-06 19:22:52 -0500

    added test use case for ListLessonInfoToVOMapper

--- data/src/test/java/co/anbora/labs/engvid/data/local/mapper/ListLessonInfoToVOMapperTest.java
+++ data/src/test/java/co/anbora/labs/engvid/data/local/mapper/ListLessonInfoToVOMapperTest.java
@@ -1,12 +1,45 @@
 package co.anbora.labs.engvid.data.local.mapper;
 
+import co.anbora.labs.engvid.data.local.model.LessonInfoVO;
+import co.anbora.labs.engvid.domain.model.lesson.LessonInfo;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class ListLessonInfoToVOMapperTest {
 
+    private ListLessonInfoToVOMapper mapper;
+
     @Before
     public void setUp() throws Exception {
+
+        mapper = new ListLessonInfoToVOMapper(new LessonInfoToVOMapper());
+    }
+
+    @Test
+    public void givenAListLessonNullReturnNull() {
+
+        Assert.assertNull(mapper.apply(null));
+    }
+
+    @Test
+    public void givenAListLessonReturnListLessonVO() {
+
+        LessonInfo lessonInfo = LessonInfo.builder()
+                .id(1)
+                .title("test")
+                .description("test")
+                .build();
+
+        LessonInfoVO lessonInfoVO = new LessonInfoVO(
+                1, "test", "test", null, null, null, null
+        );
+
+        Assert.assertEquals(Arrays.asList(lessonInfoVO), mapper.apply(Arrays.asList(lessonInfo)));
     }
 }
