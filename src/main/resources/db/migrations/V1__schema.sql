fe2f49950d790afc46fc3bbddf8b4e6315054958
Restyled.io <commits@restyled.io>
2020-03-29 21:51:59 +0000

    Restyled by pg_format

--- src/main/resources/db/migrations/V1__schema.sql
+++ src/main/resources/db/migrations/V1__schema.sql
@@ -1,21 +1,23 @@
 DROP TABLE IF EXISTS lessons;
+
 DROP TABLE IF EXISTS categories;
 
 CREATE TABLE categories (
-    category_id BIGINT        PRIMARY KEY NOT NULL,
-    name_ VARCHAR(255)        NOT NULL
+    category_id BIGINT PRIMARY KEY NOT NULL,
+    name_ VARCHAR(255) NOT NULL
 );
 
 CREATE TABLE lessons (
-  lesson_id BIGINT               PRIMARY KEY NOT NULL,
-  title VARCHAR(255)             NOT NULL,
-  description VARCHAR(4096)      NOT NULL,
-  publish_date VARCHAR(255)      NOT NULL,
-  render_link VARCHAR(255)       NOT NULL,
-  category_ BIGINT               NOT NULL,
-  slug VARCHAR(255)              NOT NULL,
-  image_url VARCHAR(255)         NOT NULL DEFAULT 'https://www.engvid.com/wp-content/themes/engvid2/images/engvid.png',
-  youtube_id VARCHAR(255)        ,
-  sync BOOLEAN                   NOT NULL DEFAULT false,
-  constraint FKM1T3YVW5I7OLWDF32CWUUL7TA foreign key (category_) references categories(category_id)
+    lesson_id BIGINT PRIMARY KEY NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description VARCHAR(4096) NOT NULL,
+    publish_date VARCHAR(255) NOT NULL,
+    render_link VARCHAR(255) NOT NULL,
+    category_ BIGINT NOT NULL,
+    slug VARCHAR(255) NOT NULL,
+    image_url VARCHAR(255) NOT NULL DEFAULT 'https://www.engvid.com/wp-content/themes/engvid2/images/engvid.png',
+    youtube_id VARCHAR(255),
+    sync BOOLEAN NOT NULL DEFAULT FALSE,
+    CONSTRAINT FKM1T3YVW5I7OLWDF32CWUUL7TA FOREIGN KEY (category_) REFERENCES categories (category_id)
 );
+
